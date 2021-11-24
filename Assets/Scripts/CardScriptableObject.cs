d0aa79e0e1bc87d21f5128b9f1aeb91ccf9f0431
ggwp313 <nemchuk.roman@gmail.com>
2021-11-24 12:02:14 +0100

    Cards_Scriptable_Objects

---
+++ Assets/Scripts/CardScriptableObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu( fileName = "Create New Card", menuName = "Cards/Minion Card" )]
+public class CardScriptableObject : ScriptableObject
+{
+    [Header("Card Info")]
+    public string cardName;
+    public string cardDescription;
+    public int cardManacost;
+
+    [Header("Card Stats")]
+    public int cardAttack;
+    public int cardHealth;
+
+    [Header("Card Art")]
+    public Sprite cardArt;
+    public Sprite cardBackground;
+}
---
+++ Assets/Scripts/CardUIDisplay.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardUIDisplay : MonoBehaviour
+{
+    [SerializeField] 
+    private CardScriptableObject _cardInfo;
+
+    [SerializeField]
+  public TextMeshProUGUI _cardName;
+    [SerializeField]
+    private TextMeshProUGUI _cardDescription;
+    [SerializeField]
+    private TextMeshProUGUI _cardAttack;
+    [SerializeField]
+    private TextMeshProUGUI _cardHealth;
+    [SerializeField]
+    private TextMeshProUGUI _cardManacost;
+
+    [SerializeField]
+    private Image _cardArt;
+    [SerializeField]
+    private Image _cardBackgroundImage;
+
+    void Start()
+    {
+        CardInitialize(); 
+    }
+
+    private void CardInitialize()
+    {
+        _cardName.text = _cardInfo.cardName;
+        _cardDescription.text = _cardInfo.cardDescription;
+        _cardAttack.text = _cardInfo.cardAttack.ToString();
+        _cardHealth.text = _cardInfo.cardHealth.ToString();
+        _cardManacost.text = _cardInfo.cardManacost.ToString();
+        _cardArt.sprite = _cardInfo.cardArt;
+        _cardBackgroundImage.sprite = _cardInfo.cardBackground;
+    }
+}
